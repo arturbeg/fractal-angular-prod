be624f68a011c79174dc216a09d921db576f1aa0
jay <jayzern@hotmail.com>
2018-03-27 02:33:00 +0800

    update: moved material imports away

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
+// Components
+import { AppRoutingModule } from './/app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -9,43 +10,20 @@ import { ProfileComponent } from './profile/profile.component';
 import { RecentActivityComponent } from './recent-activity/recent-activity.component';
 import { ChatComponent } from './chat/chat.component';
 import { ChatgroupComponent } from './chatgroup/chatgroup.component';
-
-// Needed for animations
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
-
-// Importing NgBootstrap's main module
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-
 import { ChatCardComponent } from './chat-card/chat-card.component';
-
-// Material related components
-
-import {MatCardModule} from '@angular/material/card';
-
-import {MatButtonModule} from '@angular/material/button';
-
-
-import {MatIconModule} from '@angular/material/icon';
-
-import {MatMenuModule} from '@angular/material/menu';
-
-import {MatFormFieldModule} from '@angular/material/form-field';
-
-import {MatInputModule} from '@angular/material/input';
-
-import {MatTabsModule} from '@angular/material/tabs';
-
-
-import { AppRoutingModule } from './/app-routing.module';
 import { MessageComponent } from './message/message.component';
 import { PostComponent } from './post/post.component';
 import { PostCommentComponent } from './post-comment/post-comment.component';
 import { ResultsComponent } from './results/results.component';
 import { ChatgroupCardComponent } from './chatgroup-card/chatgroup-card.component';
 import { UserCardComponent } from './user-card/user-card.component';
+
+// Material + animations
+import { MaterialModule } from './material.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+// Importing NgBootstrap's main module
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 
 @NgModule({
@@ -69,20 +47,8 @@ import { UserCardComponent } from './user-card/user-card.component';
     BrowserModule,
     BrowserAnimationsModule,
     NgbModule.forRoot(),
-    
-    // Material related imports
-    MatCardModule,
-    MatButtonModule,
-    MatIconModule,
-    MatMenuModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatTabsModule,
-
-
     AppRoutingModule,
-
-
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/material.module.ts
@@ -0,0 +1,45 @@
+import { NgModule } from '@angular/core';
+
+import {
+  MatButtonModule,
+  MatMenuModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatDividerModule,
+  MatListModule
+} from '@angular/material';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+
+@NgModule({
+  imports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
+  ],
+  exports: [
+    MatButtonModule,
+    MatMenuModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+    MatTabsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCheckboxModule
+  ]
+})
+export class MaterialModule {}
