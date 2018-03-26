8477c34aa88323ec42293dcc4427a5aaa58952d5
jay <jayzern@hotmail.com>
2018-03-27 02:41:56 +0800

    update: change route from home to /

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,31 +3,21 @@ import { CommonModule } from '@angular/common';
 
 
 import { RouterModule, Routes } from '@angular/router';
-
-
 import { ChatComponent } from './chat/chat.component';
-
 import { HomeComponent } from './home/home.component';
-
 import { ProfileComponent } from './profile/profile.component';
-
 import { ChatgroupComponent } from './chatgroup/chatgroup.component';
-
 import { RecentActivityComponent } from './recent-activity/recent-activity.component';
-
 import { ResultsComponent } from './results/results.component';
 
 const routes: Routes = [
-  
 
+  { path: '', component: HomeComponent },
   { path: 'chat', component: ChatComponent },
-  { path: 'home', component: HomeComponent },
   { path: 'profile', component: ProfileComponent },
   { path: 'chatgroup', component: ChatgroupComponent },
   { path: 'recentactivity', component: RecentActivityComponent },
   { path: 'results', component: ResultsComponent },
-
-
 
 ];
 
