82edf5006b75daec21923aace4623f53cd103e21
Artur Begyan <arturbegyan98@gmail.com>
2018-09-05 14:09:43 +0100

    minor fixes to package.json, gitignore

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
-const express= require('express'),
-path = require('path');
+//Install express server
+const express = require('express');
+const path = require('path');
 
-const app =express();
+const app = express();
 
+// Serve only the static files form the dist directory
 app.use(express.static('./dist/fractal-angular'));
 
-app.get('/*', (req,res)=>{
- 
+app.get('/*', function(req,res) {
+    
 res.sendFile(path.join(__dirname,'/dist/fractal-angular/index.html'));
-
 });
 
-app.listen(process.env.PORT || 8080, ()=>{
-console.log('Server started');
-})
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
