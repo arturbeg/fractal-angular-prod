3084767ea27065cd38a0cf4e90723db7aceb7a19
Artur Begyan <arturbegyan98@gmail.com>
2018-09-05 02:14:23 +0100

    minor

--- server.js
+++ server.js
@@ -1,16 +1,34 @@
-//Install express server
 const express = require('express');
+const http = require('http')
 const path = require('path');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/fractal-angular'));
+app.use(express.static(path.join(__dirname, 'dist')));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/fractal-angular/index.html'));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+server.listen(port, () => console.log('running'));
+
+// //Install express server
+// const express = require('express');
+// const path = require('path');
+
+// const app = express();
+
+// // Serve only the static files form the dist directory
+// app.use(express.static(__dirname + '/dist/fractal-angular'));
+
+// app.get('/*', function(req,res) {
+    
+// res.sendFile(path.join(__dirname+'/dist/fractal-angular/index.html'));
+// });
+
+// // Start the app by listening on the default Heroku port
+// app.listen(process.env.PORT || 8080);
