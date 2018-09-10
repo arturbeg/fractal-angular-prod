8a8d705043e7f6c29eff02745db39822135058ff
Artur Begyan <arturbegyan98@gmail.com>
2018-09-10 17:10:21 +0100

    so smth

--- server.js
+++ server.js
@@ -5,11 +5,14 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/fractal-angular'));
+app.use(express.static('./dist'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/fractal-angular/index.html'));
+  console.log("SERVER LOADED")
+  res.sendFile(path.join(__dirname,'/dist/index.html'));
+
+
 });
 
 // Start the app by listening on the default Heroku port
